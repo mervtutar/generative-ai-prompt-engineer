bdc2e1c96f38f5b2ad3b1746562e6214674f594d
Merve Tutar <mervetutar78@gmail.com>
2024-09-27 23:42:59 +0300

    text generation

--- text101/command.py
+++ text101/command.py
@@ -8,16 +8,33 @@ my_key = os.environ.get("cohere_apikey")
 
 client = cohere.Client( api_key=my_key)
 
-AI_Response = client.chat(
-    model = "command",
-    temperature=0,
-    max_tokens=256,
-    chat_history=[
-        {"role": "USER","message":"Adı Hürjet olan uçak nereye aittir?"},
-        {"role":"CHATBOT","message":"Hürjet Türkiye'ye ait bir jet eğitim ve yakın hava desteği uçagıdır."}
+def generate_response(prompt):
 
-    ],
-    message="Hangi amaçla geliştirilmiştir?"
-)
+    AI_Response = client.chat(
+        model = "command",
+        temperature=0,
+        max_tokens=256,
+        chat_history=[
+            {"role": "USER","message":"Adı Hürjet olan uçak nereye aittir?"},
+            {"role":"CHATBOT","message":"Hürjet Türkiye'ye ait bir jet eğitim ve yakın hava desteği uçagıdır."}
 
-print(AI_Response.text)
+        ],
+        message=prompt
+    )
+
+    return AI_Response.text
+
+import streamlit as st
+
+st.header("Command ile sohbet edin")
+st.divider()
+
+
+prompt = st.text_input("Mesajınızı giriniz:")
+submit_buton = st.button("gönder")
+
+if submit_buton:
+
+    response=generate_response(prompt)
+    st.markdown(response)
+    
